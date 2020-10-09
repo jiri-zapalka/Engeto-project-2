43eaffde96f28bf2dcaa9c32ad3db06a65aca568
Jiří Zápalka <zapalka.jiri@gmail.com>
2020-10-09 21:46:41 +0200

    Winner check add & program ending. Tic Tac Toe is now working.

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
+from play_field import tic_tac_toe
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("""Welcome to Tic Tac Toe
+GAME RULES:
+Each player can place one mark (or stone) per turn on the 3x3 grid
+The WINNER is who succeeds in placing three of their marks in a
+* horizontal,
+* vertical or
+* diagonal row
+Play field position:
+-------------------- 
+  1  |  2  |   3  
+  4  |  5  |   6  
+  7  |  8  |   9   
+--------------------
+Let's start the game""")
+tic_tac_toe()
--- play_field.py
+++ play_field.py
@@ -1,43 +1,58 @@
-separator = (20 * "-")
-start = {"a": "", "b": "", "c": "", "d": "", "e": "", "f": "", "g": "",
-         "h": "", "i": ""}
-players = (1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2)
-index = 0
-# Players switching
-while players:
-    player = players[index]
-    print(player)
-    # Updating of Start dic. - game progress
-    while start:
-        x = int(input("Place your token pos. 1-9: ")) - 1
-        # Checking of correct input value
-        if x in range(0, 8):
-            arg = "abcdefghi"
-            play_arg = arg[x]
-            # Deciding if the token will be marked as X or O based on active player
-            if play_arg in start.keys():
-                if player == 1:
-                    start[play_arg] = "X"
-                else:
-                    start[play_arg] = "O"
-        # In case that player place higher number
-        else:
-            print("Wrong pos. number. PLAY AGAIN or you loose your turn.")
+def tic_tac_toe():
+    separator = (20 * "-")
+    s = {"a": "", "b": "", "c": "", "d": "", "e": "",
+         "f": "", "g": "", "h": "", "i": ""}
+    players = (1, 2) * 20
+    index = 0
+    # Players switching
+    while players:
+        player = players[index]
+        print(player)
+        # Updating of Start dic. - game progress
+        while s:
             x = int(input("Place your token pos. 1-9: ")) - 1
             arg = "abcdefghi"
-            play_arg = arg[x]
-            # Deciding if the token will be marked as X or O based on active player
-            if play_arg in start.keys():
-                if player == 1:
-                    start[play_arg] = "X"
-                else:
-                    start[play_arg] = "O"
-        # Creating a play grid
-        grid = "{0:^4}|{1:^5}|{2:^5}\n"\
-               "{3:^5}|{4:^5}|{5:^5}\n"\
-               "{6:^5}|{7:^5}|{8:^5}".format(start["a"], start["b"], start["c"],
-                                             start["d"], start["e"], start["f"],
-                                             start["g"], start["h"], start["i"])
-        print("\n", grid, "\n")
-        break
-    index += 1
+            p_arg = arg[x]
+            # Checking of correct input value
+            if x in range(0, 9) and s.get(p_arg) == "":
+                # Deciding if the token will be marked as X or O based on active player
+                if p_arg in s.keys():
+                    if player == 1:
+                        s[p_arg] = "X"
+                    else:
+                        s[p_arg] = "O"
+            # In case that player place higher number
+            else:
+                print("Wrong pos. number. PLAY AGAIN or you loose your turn.")
+                x = int(input("Place your token pos. 1-9: ")) - 1
+                arg = "abcdefghi"
+                p_arg = arg[x]
+                # Deciding if the token will be marked as X or O based on active player
+                if p_arg in s.keys() and s.get(p_arg) == "":
+                    if player == 1:
+                        s[p_arg] = "X"
+                    else:
+                        s[p_arg] = "O"
+            # Creating a play grid
+            grid = "{0:^4}|{1:^5}|{2:^5}\n"\
+                   "{3:^5}|{4:^5}|{5:^5}\n"\
+                   "{6:^5}|{7:^5}|{8:^5}".format(s["a"], s["b"], s["c"],
+                                                 s["d"], s["e"], s["f"],
+                                                 s["g"], s["h"], s["i"])
+            print(separator, "\n", grid, "\n", separator)
+            # winner check
+            if s["a"] == s[p_arg] and s["b"] == s[p_arg] and s["c"] == s[p_arg] \
+                    or s["d"] == s[p_arg] and s["e"] == s[p_arg] and s["f"] == s[p_arg] \
+                    or s["g"] == s[p_arg] and s["h"] == s[p_arg] and s["i"] == s[p_arg] \
+                    or s["a"] == s[p_arg] and s["d"] == s[p_arg] and s["g"] == s[p_arg] \
+                    or s["b"] == s[p_arg] and s["e"] == s[p_arg] and s["h"] == s[p_arg] \
+                    or s["c"] == s[p_arg] and s["f"] == s[p_arg] and s["i"] == s[p_arg] \
+                    or s["a"] == s[p_arg] and s["e"] == s[p_arg] and s["i"] == s[p_arg] \
+                    or s["c"] == s[p_arg] and s["e"] == s[p_arg] and s["g"] == s[p_arg]:
+                print("!!!!Player ", s[p_arg], " WIN!!!!")
+                exit()
+            else:
+                print("Next round.")
+            break
+        index += 1
+    return tic_tac_toe()
